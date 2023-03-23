f1d9c13bd653eef1e17e641c6e7cdad139c7cf45
14932 <127011285+14932@users.noreply.github.com>
2023-03-23 19:17:36 +0500

    added models

---
+++ API/API/DataDbContext/MusicDbContext.cs
@@ -0,0 +1,13 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.DataDbContext
+{
+    public class MusicDbContext : DbContext
+    {
+        public MusicDbContext(DbContextOptions<MusicDbContext> options) : base(options) { Database.EnsureCreated(); }
+
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
---
+++ API/API/Models/Song.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AlbumName { get; set; }
+        public string AuthorName { get; set; }
+    }
+}
